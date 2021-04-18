81b45bf677edf012904c8f1bafa0437c52baaec6
mirzalf01 <irzalfahriza0@gmail.com>
2021-04-18 12:31:26 +0700

    Style: Change result view

--- app/src/main/java/com/google/firebase/codelab/translator/ResultActivity.java
+++ app/src/main/java/com/google/firebase/codelab/translator/ResultActivity.java
@@ -3,25 +3,54 @@ package com.google.firebase.codelab.translator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
-    TextView fname;
-    TextView fket;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
 
         Intent intent = getIntent();
         final String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        fname = (TextView)findViewById(R.id.fName);
-        fket = (TextView)findViewById(R.id.fket);
+        textResult = (TextView)findViewById(R.id.textResult);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        dropdown.getBackground().setColorFilter(getResources().getColor(R.color.black), PorterDuff.Mode.SRC_ATOP);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.language_array, R.layout.dropdownlang);
+        adapter.setDropDownViewResource(R.layout.dropdownlang);
+        dropdown.setAdapter(adapter);
 
-        fname.setText("Translate Result");
-        fket.setText(message);
+        textResult.setText(message);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                dropdown.setOnItemSelectedListener(this);
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: "+item, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
