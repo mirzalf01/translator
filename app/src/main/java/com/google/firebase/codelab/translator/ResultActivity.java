ba7373e87cd7017fe028ed93488c64f1c947921f
mirzalf01 <irzalfahriza0@gmail.com>
2021-04-19 08:35:56 +0700

    Fix: Fixing bug in result page

--- app/src/main/java/com/google/firebase/codelab/translator/ResultActivity.java
+++ app/src/main/java/com/google/firebase/codelab/translator/ResultActivity.java
@@ -1,7 +1,10 @@
 package com.google.firebase.codelab.translator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Build;
@@ -12,39 +15,82 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.common.model.DownloadConditions;
+import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
+
+import dmax.dialog.SpotsDialog;
 
 public class ResultActivity extends AppCompatActivity {
 
     TextView textResult;
+    TextView textSourceID;
+    public static Context context;
+    AlertDialog waitingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        context = getApplicationContext();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
+        waitingDialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Download Model")
+                .setCancelable(false).build();
         Intent intent = getIntent();
-        final String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        final String[] arr = intent.getStringArrayExtra(MainActivity.EXTRA_MESSAGE);
         textResult = (TextView)findViewById(R.id.textResult);
+        textSourceID = (TextView)findViewById(R.id.langSource);
         Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
         dropdown.getBackground().setColorFilter(getResources().getColor(R.color.black), PorterDuff.Mode.SRC_ATOP);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.language_array, R.layout.dropdownlang);
         adapter.setDropDownViewResource(R.layout.dropdownlang);
         dropdown.setAdapter(adapter);
-
-        textResult.setText(message);
+        String[] message = {TranslateLanguage.INDONESIAN,arr[1],};
+        textSourceID.setText("Lang ID : "+arr[0]);
+        textResult.setText(arr[1]);
         dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                waitingDialog.show();
                 dropdown.setOnItemSelectedListener(this);
-                String item = parent.getItemAtPosition(position).toString();
-                Toast.makeText(parent.getContext(), "Selected: "+item, Toast.LENGTH_SHORT).show();
+                String lang = parent.getItemAtPosition(position).toString();
+                String langTarget = "";
+                switch(lang){
+                    case "German":
+                        langTarget = TranslateLanguage.GERMAN;
+                        break;
+                    case "English":
+                        langTarget = TranslateLanguage.ENGLISH;
+                        break;
+                    case "Spain":
+                        langTarget = TranslateLanguage.SPANISH;
+                        break;
+                    case "Indonesia":
+                        langTarget = TranslateLanguage.INDONESIAN;
+                        break;
+                    case "Portuguese":
+                        langTarget = TranslateLanguage.PORTUGUESE;
+                        break;
+                    case "Tagalog":
+                        langTarget = TranslateLanguage.TAGALOG;
+                        break;
+                }
+                translateText(message, langTarget);
             }
 
             @Override
@@ -52,5 +98,56 @@ public class ResultActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void translateText(String[] arr, String langTarget) {
+        TranslatorOptions options =
+                new TranslatorOptions.Builder()
+                        .setSourceLanguage(arr[0])
+                        .setTargetLanguage(langTarget)
+                        .build();
+        final Translator languageTranslator =
+                Translation.getClient(options);
+        DownloadConditions conditions = new DownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        languageTranslator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void v) {
+                                languageTranslator.translate(arr[1])
+                                        .addOnSuccessListener(
+                                                new OnSuccessListener<String>() {
+                                                    @Override
+                                                    public void onSuccess(@NonNull String translatedText) {
+                                                        if (waitingDialog.isShowing())
+                                                            waitingDialog.dismiss();
+                                                        textResult.setText(translatedText);
+
+                                                    }
+                                                })
+                                        .addOnFailureListener(
+                                                new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        arr[0] = "Er";
+                                                        arr[1] = "Translate Error!";
+                                                        textResult.setText(arr[1]);
+                                                    }
+                                                });
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                arr[0] = "Er";
+                                arr[1] = "Download Model Error!";
+                                textResult.setText(arr[1]);
+                            }
+                        });
+
+
     }
 }
--- app/src/main/java/com/google/firebase/codelab/translator/Utils.java
+++ app/src/main/java/com/google/firebase/codelab/translator/Utils.java
@@ -21,58 +21,80 @@ import java.util.List;
 public class Utils {
     public static final String EXTRA_MESSAGE = "com.google.firebase.codelab.translator.extra.MESSAGE";
     public static void processTextRecognitionResult(Text texts, Activity activity, Context context){
+        String[] arr = new String[2];
         List<Text.TextBlock> blocks = texts.getTextBlocks();
         if (blocks.size() == 0) {
-            showResult("Block size = 0!", activity, context);
+            arr[0] = "Er";
+            arr[1] = "Block size = 0!";
+            showResult(arr, activity, context);
         }
         String langID = blocks.get(0).getRecognizedLanguage();
-        String langSource = cekLanguage(langID);
-        if (langSource == "unknown"){
-            showResult("Bahasa asal tidak dikenal \n Text asal : \n"+texts.getText(), activity, context);
-        }
         final String sourceText = texts.getText();
-        TranslatorOptions options =
-                new TranslatorOptions.Builder()
-                        .setSourceLanguage(langSource)
-                        .setTargetLanguage(TranslateLanguage.INDONESIAN)
-                        .build();
-        final Translator languageTranslator =
-                Translation.getClient(options);
-        DownloadConditions conditions = new DownloadConditions.Builder()
-                .requireWifi()
-                .build();
-        languageTranslator.downloadModelIfNeeded(conditions)
-                .addOnSuccessListener(
-                        new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void v) {
-                                languageTranslator.translate(sourceText)
-                                        .addOnSuccessListener(
-                                                new OnSuccessListener<String>() {
-                                                    @Override
-                                                    public void onSuccess(@NonNull String translatedText) {
-                                                        showResult(translatedText, activity, context);
-                                                    }
-                                                })
-                                        .addOnFailureListener(
-                                                new OnFailureListener() {
-                                                    @Override
-                                                    public void onFailure(@NonNull Exception e) {
-                                                        showResult("Translate Error!", activity, context);
-                                                    }
-                                                });
-                            }
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                showResult("Download Model Error!", activity, context);
-                            }
-                        });
+        arr[0] = langID;
+        arr[1] = sourceText;
+        String targetLang = TranslateLanguage.INDONESIAN;
+        translateText(arr, targetLang, activity, context);
     }
 
-    private static String cekLanguage(String langID) {
+    public static void translateText(String[] arr, String targetLang, Activity activity, Context context) {
+        String langSource = cekLanguage(arr[0]);
+        if (langSource == "unknown"){
+            arr[0] = "Er";
+            arr[1] = "Bahasa asal tidak dikenal";
+            showResult(arr, activity, context);
+        }
+        else{
+            TranslatorOptions options =
+                    new TranslatorOptions.Builder()
+                            .setSourceLanguage(langSource)
+                            .setTargetLanguage(targetLang)
+                            .build();
+            final Translator languageTranslator =
+                    Translation.getClient(options);
+            DownloadConditions conditions = new DownloadConditions.Builder()
+                    .requireWifi()
+                    .build();
+            languageTranslator.downloadModelIfNeeded(conditions)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void v) {
+                                    languageTranslator.translate(arr[1])
+                                            .addOnSuccessListener(
+                                                    new OnSuccessListener<String>() {
+                                                        @Override
+                                                        public void onSuccess(@NonNull String translatedText) {
+                                                            arr[0] = langSource;
+                                                            arr[1] = translatedText;
+                                                            showResult(arr, activity, context);
+
+                                                        }
+                                                    })
+                                            .addOnFailureListener(
+                                                    new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+                                                            arr[0] = "Er";
+                                                            arr[1] = "Translate Error!";
+                                                            showResult(arr, activity, context);
+                                                        }
+                                                    });
+                                }
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    arr[0] = "Er";
+                                    arr[1] = "Download Model Error!";
+                                    showResult(arr, activity, context);
+                                }
+                            });
+        }
+
+    }
+
+    public static String cekLanguage(String langID) {
         switch (langID){
             case "de":
                 return TranslateLanguage.GERMAN;
@@ -91,9 +113,9 @@ public class Utils {
         }
     }
 
-    private static void showResult(String translatedText, Activity activity, Context context) {
+    private static void showResult(String[] arr, Activity activity, Context context) {
         Intent intent = new Intent(activity, ResultActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, translatedText);
+        intent.putExtra(EXTRA_MESSAGE, arr);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
